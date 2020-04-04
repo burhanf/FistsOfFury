9bb1a4604fb04e77e8cdb361dc836c18adc107e8
Burhan Faquiri <burhan_faquiri@hotmail.com>
2020-04-04 14:22:31 -0400

    Burhan: Implemented the abstract method BonusAttack for Shao Kahn

--- FistsOfFury/Classes/ShaoKahn.cs
+++ FistsOfFury/Classes/ShaoKahn.cs
@@ -6,8 +6,32 @@ using System.Threading.Tasks;
 
 namespace FistsOfFury.Classes
 {
-    class ShaoKahn
+    //By Burhan
+    class ShaoKahn : Fighter
     {
-        //By Burhan
+        public ShaoKahn()
+        {
+            Name = "Shao Kahn";
+        }
+        public override void BonusAttack(Fighter opponent)
+        {
+            if (!IsBonusUsed)
+            {
+                //Hammer
+                int land = DetermineIfLand();
+
+                if (land >= 8)
+                {
+                    //bare damage
+                    opponent.UpdateHealth(40);
+                    IsBonusUsed = true;
+                    //image change
+                }
+            }
+            else
+            {
+                throw new Exception("Bonus is already used");
+            }
+        }
     }
 }
