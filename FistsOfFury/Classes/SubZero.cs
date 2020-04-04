287b8d5db9e29f01f4e2774298864d139305eafc
Burhan Faquiri <burhan_faquiri@hotmail.com>
2020-04-04 14:18:50 -0400

    Burhan: Implemented the abstract method bonus move for Sub-Zero

--- FistsOfFury/Classes/SubZero.cs
+++ FistsOfFury/Classes/SubZero.cs
@@ -6,8 +6,34 @@ using System.Threading.Tasks;
 
 namespace FistsOfFury.Classes
 {
-    class SubZero
+    //By Burhan
+    class SubZero : Fighter
     {
-        //By Burhan
+        public SubZero()
+        {
+            Name = "Sub-Zero";
+        }
+        public override void BonusAttack(Fighter opponent)
+        {
+            if (!IsBonusUsed)
+            {
+                //LowPunch
+                int land = DetermineIfLand();
+
+                if (land >= 8)
+                {
+                    //gives health
+                    Health += 20;
+                    opponent.UpdateHealth(10);
+                    IsBonusUsed = true;
+                    //image change
+                }
+            }
+            else
+            {
+                throw new Exception("Bonus is already used");
+            }
+
+        }
     }
 }
