73ddcf2cd30d21836ac4e4d54ab6a85ca092aa6c
Burhan Faquiri <burhan_faquiri@hotmail.com>
2020-04-04 14:20:59 -0400

    Burhan: Implemented the abstract method BonusAttack for Scorpion

--- FistsOfFury/Classes/Scorpion.cs
+++ FistsOfFury/Classes/Scorpion.cs
@@ -6,8 +6,37 @@ using System.Threading.Tasks;
 
 namespace FistsOfFury.Classes
 {
-    class Scorpion
+    //By Burhan
+    class Scorpion : Fighter
     {
-        //By Burhan
+        public Scorpion()
+        {
+            Name = "Scorpion";
+        }
+
+        public override void BonusAttack(Fighter opponent)
+        {
+            if (!IsBonusUsed)
+            {
+                //Uppercut
+                int land = DetermineIfLand();
+
+                if (land >= 8)
+                {
+                    //gives health
+                    Points += 800;
+
+                    opponent.UpdateHealth(10);
+                    IsBonusUsed = true;
+                    //image change
+                }
+            }
+            else
+            {
+                throw new Exception("Bonus is already used");
+            }
+
+        }
+
     }
 }
