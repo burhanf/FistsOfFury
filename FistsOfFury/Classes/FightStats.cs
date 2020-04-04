b2cad02b481e8e4e57d38b0850259c78fa49de21
Burhan Faquiri <burhan_faquiri@hotmail.com>
2020-04-04 14:16:32 -0400

    Burhan: Implemented logi for tracking and updating fighter statistics.

--- FistsOfFury/Classes/FightStats.cs
+++ FistsOfFury/Classes/FightStats.cs
@@ -6,8 +6,83 @@ using System.Threading.Tasks;
 
 namespace FistsOfFury.Classes
 {
+    //By Burhan
     class FightStats
     {
-        //By Burhan
+        public double PunchesThrown { get; private set; }
+        public double HighKicksThrown { get; private set; }
+        public double LowKicksThrown { get; private set; }
+        public double PunchesLanded { get; private set; }
+        public double HighKicksLanded { get; private set; }
+        public double LowKicksLanded { get; private set; }
+        public double PunchAccuracy
+        {
+            get
+            {
+                if (PunchesThrown != 0)
+                {
+                    return PunchesLanded / PunchesThrown * 100;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+        public double LowKickAccuracy
+        {
+            get
+            {
+                if (LowKicksThrown != 0)
+                {
+                    return LowKicksLanded / LowKicksThrown * 100;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+        public double HighKickAccuracy
+        {
+            get
+            {
+                if (HighKicksThrown != 0)
+                {
+                    return HighKicksLanded / HighKicksThrown * 100;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+        //methods
+        public void UpdatePunchThrown()
+        {
+            PunchesThrown++;
+        }
+        public void UpdateHighKicksThrown()
+        {
+            HighKicksThrown++;
+        }
+        public void UpdateLowKicksThrown()
+        {
+            LowKicksThrown++;
+        }
+        public void UpdatePunchLanded()
+        {
+            PunchesLanded++;
+        }
+        public void UpdateHighKicksLanded()
+        {
+            HighKicksLanded++;
+        }
+        public void UpdateLowKicksLanded()
+        {
+            LowKicksLanded++;
+        }
+
     }
+
 }
