e3e6dab2c67e3f3d788410b1ede3e9d02a3bd9e0
sicilima <sicilima@shernet.sheridancollege.ca>
2020-04-09 22:35:59 -0400

    Organization

--- FistsOfFury/Classes/Database/Database.cs
+++ FistsOfFury/Classes/Database/Database.cs
@@ -12,14 +12,14 @@ namespace FistsOfFury.Classes
     public class Database
     {
         private MongoClient _client { get; }
-        private IMongoDatabase _database { get; }
+        private IMongoDatabase _db { get; }
         private IMongoCollection<BsonDocument> _leaderboard { get; set; }
 
         public Database()
         {
             _client = new MongoClient("mongodb+srv://admin:admin@cluster-suzyschema-f4pio.azure.mongodb.net/test?retryWrites=true&w=majority");
-            _database = _client.GetDatabase("Fists-Of-Fury");
-            _leaderboard = _database.GetCollection<BsonDocument>("Leaderboard");
+            _db = _client.GetDatabase("Fists-Of-Fury");
+            _leaderboard = _db.GetCollection<BsonDocument>("Leaderboard");
         }
          
         public async void LogMatch(Match match)
@@ -28,6 +28,16 @@ namespace FistsOfFury.Classes
             {
                 await _leaderboard.InsertOneAsync(BsonDocument.Parse(JsonConvert.SerializeObject(stat)));
             }
+        }
+
+        public List<BsonDocument> GetLeaderboard()
+        {
+            List<BsonDocument> myLeaders = new List<BsonDocument>();
+            foreach (BsonDocument document in _leaderboard.Find(new BsonDocument()).ToList())
+            {
+                myLeaders.Add(document);
+            }
+            return myLeaders;
         }
     }
 }
--- FistsOfFury/Pages/MainPage.xaml.cs
+++ FistsOfFury/Pages/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using FistsOfFury.Classes;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -10,15 +11,23 @@ namespace FistsOfFury.Pages
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        Database Database { get; }
+        
         public MainPage()
         {
             this.InitializeComponent();
+            Database = new Database();
         }
 
         private void PlayButton_OnClick(object sender, RoutedEventArgs e)
         {
             //go to the character selection screen
-            this.Frame.Navigate(typeof(CharacterSelection));
+            this.Frame.Navigate(typeof(CharacterSelection), Database);
+        }
+
+        private void LeaderboardButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Leaderboard), Database);
         }
     }
 }
