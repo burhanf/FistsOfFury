f3071b9e67d5049bd74514a3ca54b6f93506685e
Justin Lam <Justin_lam13@hotmail.com>
2020-04-17 18:07:35 -0400

    Justin's Stage Screen Addition

---
+++ FistsOfFury/Classes/Maps.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace FistsOfFury.Classes
+{
+    abstract class Maps
+    {
+        public string ImageName { get; set; }
+
+        public Image MImage { get; set; }
+
+    }
+}
---
+++ FistsOfFury/Pages/StageScreen.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using FistsOfFury.Classes;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace FistsOfFury.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class StageScreen : Page
+    {
+        public StageScreen()
+        {
+            this.InitializeComponent();
+
+            PopulateStages();
+
+            NavigationCacheMode = NavigationCacheMode.Enabled;
+        }
+
+        public void PopulateStages()
+        {
+            Courtyard courtyard = new Courtyard();
+            ThePit thePit = new ThePit();
+            DeadPool deadPool = new DeadPool();
+
+            List<Maps> maps = new List<Maps>();
+            maps.Add(deadPool);
+            maps.Add(courtyard);
+            maps.Add(thePit);
+            foreach (var Maps in maps)
+            {
+                Stages.Items.Add(maps);
+            }
+
+        }
+
+        private void GoToBattle_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(FightingPage));
+        }
+
+        private void ReturnToCharacters_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Frame.GoBack();
+        }
+
+    }
+}
