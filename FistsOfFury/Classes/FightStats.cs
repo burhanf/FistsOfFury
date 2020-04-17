1283e32150a12dd84e2de4fd3c55332dd67d1557
sicilima <sicilima@shernet.sheridancollege.ca>
2020-04-16 23:18:31 -0400

    Swap if statements for turnary operators

--- FistsOfFury/Classes/FightStats.cs
+++ FistsOfFury/Classes/FightStats.cs
@@ -16,50 +16,11 @@ namespace FistsOfFury.Classes
         public double PunchesLanded { get; private set; }
         public double HighKicksLanded { get; private set; }
         public double LowKicksLanded { get; private set; }
-        public double PunchAccuracy
-        {
-            //todo is a private variable required to be declared for this property that has a calculated getter?
-            //calculation is done in getter
-            get
-            {
-                if (PunchesThrown != 0)
-                {
-                    return PunchesLanded / PunchesThrown * 100;
-                }
-                else
-                {
-                    return 0;
-                }
-            }
-        }
-        public double LowKickAccuracy
-        {
-            get
-            {
-                if (LowKicksThrown != 0)
-                {
-                    return LowKicksLanded / LowKicksThrown * 100;
-                }
-                else
-                {
-                    return 0;
-                }
-            }
-        }
-        public double HighKickAccuracy
-        {
-            get
-            {
-                if (HighKicksThrown != 0)
-                {
-                    return HighKicksLanded / HighKicksThrown * 100;
-                }
-                else
-                {
-                    return 0;
-                }
-            }
-        }
+        //todo is a private variable required to be declared for this property that has a calculated getter?
+        //calculation is done in getter
+        public double PunchAccuracy{get { PunchesThrown <= 0 ?return PunchesLanded / PunchesThrown:return 0;}}
+        public double LowKickAccuracy{get{LowKicksThrown >= 0 ?return LowKicksLanded / LowKicksThrown:return 0;}}
+        public double HighKickAccuracy {get{HighKicksThrown >= 0 ?return HighKicksLanded / HighKicksThrown:return 0;}}
         //methods
         public void UpdatePunchThrown()
         {
