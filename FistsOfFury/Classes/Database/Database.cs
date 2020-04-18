88b54e82a15f47f505bf0f8c08daf35a1a3a975b
sicilima <sicilima@shernet.sheridancollege.ca>
2020-04-18 19:15:21 -0400

    Database fixup

--- FistsOfFury/Classes/Database/Database.cs
+++ FistsOfFury/Classes/Database/Database.cs
@@ -35,8 +35,7 @@ namespace FistsOfFury.Classes
         public List<BsonDocument> GetLeaderboard()
         {
             var sort = Builders<BsonDocument>.Sort.Descending("Score");
-            return (_leaderboard.Find(new BsonDocument()).Sort(sort)).ToList();
-
+            return _leaderboard.Find(new BsonDocument()).Sort(sort).ToList();
         }
 
         public List<BsonDocument> GetUserHistory(string name)
@@ -46,9 +45,9 @@ namespace FistsOfFury.Classes
             return _leaderboard.Find(filter).ToList();
         }
 
-        public FightStats Deserialize(BsonDocument d)
+        public void testit()
         {
-            return new FightStats(d.GetValue("FighterName").ToString(), d.GetValue("Score").ToInt32(), d.GetValue("PunchesThrown").ToInt32(), d.GetValue("HighKicksThrown").ToInt32(), d.GetValue("LowKicksThrown").ToInt32(), d.GetValue("PunchesLanded").ToInt32(), d.GetValue("HighKicksLanded").ToInt32(), d.GetValue("LowKicksLanded").ToInt32());
+            return new FightStats(d.GetValue("Name").ToString(), d.GetValue("Score").ToInt32(), d.GetValue("PunchesThrown").ToInt32(), d.GetValue("HighKicksThrown").ToInt32(), d.GetValue("LowKicksThrown").ToInt32(), d.GetValue("PunchesLanded").ToInt32(), d.GetValue("HighKicksLanded").ToInt32(), d.GetValue("LowKicksLanded").ToInt32());
         }
     }
 }
